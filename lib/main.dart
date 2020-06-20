daf69e4e4928b875162bd65a845847549dd2565d
matheusr1 <matheusrocha_456@hotmail.com>
2020-06-20 05:34:13 -0300

    modifynig layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: " contador de pessoas ",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/original.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // para coluna defino centro.
-            children: <Widget>[
-              Text("Pessoas : 0 ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(7.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 30.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+  runApp(MaterialApp(title: " contador de pessoas ", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people=0;
+  String _infoText="pode entrar";
+
+  void _changePeople(int delta){
+    setState(() {
+        _people+=delta;
+
+        if (_people<0){
+          _infoText="Mundo invertido ?!";
+        }else if  (_people<=10){
+          _infoText="pode entrar!";
+        }else{
+          _people=10;
+          _infoText="Lotado!";
+        }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/original.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // para coluna defino centro.
+          children: <Widget>[
+            Text("Pessoas : $_people",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 30.0, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(7.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 30.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+                ),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 30.0, color: Colors.white),
                     ),
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
                   ),
-                ],
-              ),
-              Text("Pode entrar!",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      fontStyle: FontStyle.italic))
-            ],
-          )
-        ],
-      )));
+                ),
+              ],
+            ),
+            Text(_infoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic))
+          ],
+        )
+      ],
+    );
+  }
 }
